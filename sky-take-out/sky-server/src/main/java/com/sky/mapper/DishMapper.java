3e0933ea088eab6f7f4521194ab6f3d3a94b5184
morty555 <1650550685@qq.com>
2025-03-30 11:08:19 +0800

    删除菜品业务代码

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,13 +1,21 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
+
+
 
     /**
      * 根据分类id查询菜品数量
@@ -20,4 +28,12 @@ public interface DishMapper {
 
     @AutoFill(value= OperationType.INSERT)
     void insert(Dish dish);
+
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    @Select("select * from dish where id =#{id}")
+    Dish getById(Long id);
+
+    @Delete("delete from dish where id =#{id}")
+    void deleteById(Long id);
 }
